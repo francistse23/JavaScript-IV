0dfc53168591a7f06421d6d4c1d572fd6dc5a08e
francistse23 <wtse.ucsd@gmail.com>
2019-06-21 17:22:13 -0700

    Completed both assignments.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,81 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+  }
+}
+
+class Instructor extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
+  }
+
+  givePoints(student) {
+    if (student.grade < 70) {
+      return (student.grade += Math.floor(Math.random() * 10));
+    } else {
+      return (student.grade -= Math.floor(Math.random() * 10));
+    }
+  }
+}
+
+class Student extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+    this.grade = 80;
+  }
+
+  graduate(instructor) {
+    this.grade >= 70
+      ? console.log("Congratulations!")
+      : instructor.givePoints(this);
+  }
+
+  listsSubjects() {
+    console.log(this.favSubjects.forEach(subject => console.log(subject)));
+  }
+
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`);
+  }
+
+  sprintChallenge(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(attributes) {
+    super(attributes);
+    this.gradClassName = attributes.gradClassName;
+    this.favInstructor = attributes.favInstructor;
+  }
+
+  standUp(channel) {
+    console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+  }
+
+  debugCode(student, subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+  }
+}
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,62 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+class GameObject {
+  constructor(attributes) {
+    this.createdAt = attributes.createdAt;
+    this.name = attributes.name;
+    this.dimensions = attributes.dimensions;
+  }
+
+  destroy() {
+    return `${this.name} was removed from the game.`;
+  }
+}
+
+class CharacterStats extends GameObject {
+  constructor(character) {
+    super(character);
+    this.healthPoints = healthPoints;
+  }
+
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
+}
+
+class Humanoid extends CharacterStats {
+  constructor(attributes) {
+    super(attributes);
+    this.team = attributes.team;
+    this.weapons = attributes.weapons;
+    this.language = attributes.language;
+  }
+
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}`;
+  }
+}
+
+// Stretch task:
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+class Villain extends Humanoid {
+  constructor(attributes) {
+    super(attributes);
+  }
+  hit(target) {
+    target.healthPoints -= 10;
+  }
+}
+
+class Hero extends Humanoid {
+  constructor(attributes) {
+    super(attributes);
+  }
+  hit(target) {
+    target.healthPoints -= 20;
+  }
+}
